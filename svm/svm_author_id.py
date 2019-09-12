6e6130e4a02ba149bbc0bb39297f72a07a4248dd
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-09-12 23:14:59 +0530

    Incremental changes to SVM model

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,10 +25,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-clf = SVC(model="linear")
+clf = SVC(kernel="rbf", C=10000)
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+now = time()
 clf.fit(features_train, labels_train)
-result = clf.predict(featues_test)
+print("fitting time:{}".format(time()-now))
+now = time()
+result = clf.predict(features_test)
+print("prediction time:{}".format(time()-now))
 
+print("10th:{}".format(result[10]))
+print("26th:{}".format(result[26]))
+print("50th:{}".format(result[50]))
+
+from sklearn.metrics import accuracy_score
+print("accuracy: {}".format(accuracy_score(result, labels_test)))
 #########################################################
 
 
