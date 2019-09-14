80aed240884548302de2a4400aa44e3a41637216
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-09-14 23:14:41 +0530

    Tryout with Decision Tree Classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,6 +13,19 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+def calculate_accuracy(results, labels_test):
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(results, labels_test))
+
+def get_results_decision_tree(features_train, features_test, labels_train):
+    from sklearn.tree import DecisionTreeClassifier
+    clf = DecisionTreeClassifier(min_samples_split=40)
+    now = time()
+    clf.fit(features_train, labels_train)
+    print("time for training: {}".format(time()-now))
+    #print("features train: {} len:{}".format(features_train[0], len(features_train[0])))
+    results = clf.predict(features_test)
+    return results
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,8 +37,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+results = get_results_decision_tree(features_train, features_test, labels_train)
+calculate_accuracy(results, labels_test)
 #########################################################
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,37 +10,44 @@
     
 import sys
 from time import time
+import numpy as np
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+def calculate_accuracy(results, labels_test):
+    from sklearn.metrics import accuracy_score
+    print("accuracy: {}".format(accuracy_score(results, labels_test)))
+
+def get_results_svm(features_train, features_test, labels_train):
+    from sklearn.svm import SVC
+    clf = SVC(kernel="rbf", C=10000)
+    # features_train = features_train[:len(features_train)/100]
+    # labels_train = labels_train[:len(labels_train)/100]
+    now = time()
+    clf.fit(features_train, labels_train)
+    print("fitting time:{}".format(time() - now))
+    now = time()
+    result = clf.predict(features_test)
+    print(type(result))
+    print("prediction time:{}".format(time() - now))
+    count = 0
+    for res in result:
+        # print("res:{}".format(res))
+        if res == 1:
+            count += 1
+    print("count:", count)
+    return result
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-from sklearn.svm import SVC
-clf = SVC(kernel="rbf", C=10000)
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
-now = time()
-clf.fit(features_train, labels_train)
-print("fitting time:{}".format(time()-now))
-now = time()
-result = clf.predict(features_test)
-print("prediction time:{}".format(time()-now))
+result = get_results_svm(features_train, features_test, labels_train)
+calculate_accuracy(result, labels_test)
 
-print("10th:{}".format(result[10]))
-print("26th:{}".format(result[26]))
-print("50th:{}".format(result[50]))
-
-from sklearn.metrics import accuracy_score
-print("accuracy: {}".format(accuracy_score(result, labels_test)))
 #########################################################
 
 
