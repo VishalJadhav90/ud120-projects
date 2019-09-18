5850fda0aa34aa75849d75ccf8af29315b3d3235
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-09-18 22:42:20 +0530

    assigment to use your own classifier

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -2,7 +2,11 @@
 
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
-from class_vis import prettyPicture
+from class_vis import prettyPicture, output_image
+
+def accuracy(results, labels_test):
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(results, labels_test))
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -30,15 +34,29 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+clf_knn = KNeighborsClassifier(n_neighbors=5)
+clf_knn.fit(features_train, labels_train)
+results = clf_knn.predict(features_test)
+accuracy(results, labels_test)
+
+from sklearn.ensemble import RandomForestClassifier
+clf_for = RandomForestClassifier(criterion="entropy", n_estimators=5)
+clf_for.fit(features_train, labels_train)
+results = clf_for.predict(features_test)
+accuracy(results, labels_test)
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+clf_ada = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(criterion="entropy", max_depth=1), n_estimators=40)
+clf_ada.fit(features_train, labels_train)
+results = clf_ada.predict(features_test)
+accuracy(results, labels_test)
 
 
-
-
-
-
-
-
+clf = clf_ada
 try:
     prettyPicture(clf, features_test, labels_test)
+    output_image("test.png", "png", open("test.png", "rb").read())
 except NameError:
     pass
